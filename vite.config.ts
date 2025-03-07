79351f675ac25a4d36ba9f2caa9876534b28aa09
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2025-03-06 23:39:35 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',  // Final build folder
+    assetsDir: '',    // No separate assets directory (CSS will be placed under dist)
+    rollupOptions: {
+      output: {
+        // Make sure both JavaScript and CSS go into the `smarthealth` folder
+        entryFileNames: 'smarthealth/[name].js',  // JavaScript files will go into smarthealth/
+        chunkFileNames: 'smarthealth/[name].js',  // Chunks go into smarthealth/
+        assetFileNames: 'smarthealth/[name][extname]'  // CSS (and other assets) will go into smarthealth/
+      }
+    }
+  }
+});
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
